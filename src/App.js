01a676e3668ad849462997bb5c414f60a99f02c9
richardbonneau <richardbonneau91@gmail.com>
2020-08-04 21:57:06 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import "./App.css";
+const Wrapper = styled.div``;
 
-function App() {
+const HeroWrapper = styled.div`
+  position: relative;
+  width: 100%;
+`;
+const HeroOverlay = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+`;
+const Hero = styled.div`
+  background: url(/hero.jpg);
+  height: 246px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: contain;
+  /* position: absolute; */
+  top: 0;
+`;
+
+const LogoContainer = styled.a`
+  background: black;
+  height: 50px;
+  padding: 15px;
+  display: inline-block;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 10;
+`;
+const Logo = styled.img``;
+
+const Burger = styled.img`
+  width: 60px;
+  position: absolute;
+  left: 45px;
+  top: 70px;
+  width: 60px;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <HeroWrapper>
+        <HeroOverlay src="/logo-hero.png"></HeroOverlay>
+        <Hero></Hero>
+      </HeroWrapper>
+      <LogoContainer>
+        <Logo src="/logo.png" />
+      </LogoContainer>
+      <Burger src="/burg.png" />
+    </Wrapper>
   );
-}
+};
 
 export default App;
